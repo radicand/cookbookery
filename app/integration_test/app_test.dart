6eb67e84dc9b433f4173923662dc67e4a110e7ca
Nick Pappas <nick@radicand.org>
2022-07-02 19:07:06 -0500

    feat: gha workflow and dummy tests

---
+++ app/integration_test/app_test.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+// ignore: avoid_relative_lib_imports
+import '../lib/main.dart' as app;
+
+// XXX FIX ME
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  group('end-to-end test', () {
+    testWidgets('load and verify', (tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      await takeScreenshot(tester, binding, 'ss-home-1');
+
+      // Verify the counter starts at 0.
+      expect(find.text('Family Cookbook'), findsOneWidget);
+
+      // // Finds the floating action button to tap on.
+      // final Finder fab = find.byTooltip('Increment');
+
+      // // Emulate a tap on the floating action button.
+      // await tester.tap(fab);
+
+      // // Trigger a frame.
+      // await tester.pumpAndSettle();
+
+      // // Verify the counter increments by 1.
+      // expect(find.text('1'), findsOneWidget);
+    });
+  });
+}
+
+takeScreenshot(tester, binding, name) async {
+  if (Platform.isAndroid) {
+    try {
+      await binding.convertFlutterSurfaceToImage();
+    } catch (e) {
+      print("TakeScreenshot exception $e");
+    }
+    await tester.pumpAndSettle();
+  }
+
+  await binding.takeScreenshot(name);
+}
---
+++ app/test/button_test.dart
@@ -0,0 +1,17 @@
+import 'package:cookbook/widgets/button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Login prompt renders', (WidgetTester tester) async {
+    // Build widget and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: Material(
+        child:CommonButton(
+        onPressed: () {},
+        text: 'Sign In / Sign Up',
+      ))));
+
+    expect(find.text('Sign In / Sign Up'), findsOneWidget);
+  });
+}
