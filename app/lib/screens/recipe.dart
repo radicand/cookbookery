f66366cc30728ca2b6e3365773934eb1991a2d0f
Nick Pappas <nick@radicand.org>
2022-07-10 18:56:41 -0500

    fix(app): remove unused import

--- app/lib/screens/recipe.dart
+++ app/lib/screens/recipe.dart
@@ -2,7 +2,7 @@ import 'dart:developer';
 
 import 'package:cookbook/graphql_schemas/anonymous/recipe.graphql.dart';
 import 'package:cookbook/helpers/constants.dart';
-import 'package:cookbook/helpers/urls.dart';
+// import 'package:cookbook/helpers/urls.dart';
 import 'package:cookbook/widgets/rating_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
