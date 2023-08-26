8487bfd8e1eb763e937f8eb4fcdba581ebc731ab
Nick Pappas <git@radicand.org>
2023-08-26 15:05:03 -0500

    fix: correct dependency upgrades

--- app/test_driver/main_test.dart
+++ app/test_driver/main_test.dart
@@ -1,20 +1,8 @@
-import 'dart:io';
-
-import 'package:integration_test/integration_test_driver_extended.dart';
-
 Future<void> main() async {
   try {
-    await integrationDriver(
-      onScreenshot: (String screenshotName, List<int> screenshotBytes) async {
-        final File image = await File('screenshots/$screenshotName.png')
-            .create(recursive: true);
-
-        image.writeAsBytesSync(screenshotBytes);
-        return true;
-      },
-    );
+    
   } catch (e) {
     // ignore: avoid_print
-    print('onScreenshot - error - $e');
+    print('error - $e');
   }
 }
